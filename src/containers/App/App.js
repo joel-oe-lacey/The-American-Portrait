a4b83bf71614716e3181db3b916f8807e982b0a8
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-26 20:43:34 -0700

    Create test passthrough for Piece comp structuring

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { apiCall } from '../../utils/fetchCalls';
+import Piece from '../../components/Piece/Piece';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      works: {},
+      test: false
     }
   }
 
   retrieveArtPiece = async () => {
-    const piece = await fetch('https://api.harvardartmuseums.org/object/303389?apikey=b59b0050-58c4-11ea-b831-f76084e9f972')
+    const piece = await apiCall('https://api.harvardartmuseums.org/object/303389?apikey=b59b0050-58c4-11ea-b831-f76084e9f972')
     const {
       title,
       titles,
@@ -65,13 +68,16 @@ class App extends Component {
   componentDidMount() {
     //take object from data fetch, check it's returning, then try restructure through destr. 
     //to new obj for store assignment 
-    console.log(this.retrieveArtPiece())
+    this.retrieveArtPiece()
+    .then(piece => {
+      this.setState({works: piece, test: true})
+    })
   }
 
   render() {
     return (
       <div className="App">
-
+        {this.state.test && <Piece {...this.state.works} />}
       </div>
     );
   }

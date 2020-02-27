185b2d66e21a33dc55c2a8993f2583364abf8ad6
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-27 15:45:13 -0700

    Set up initial region and collection actions

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const loadRegions = (regions) => ({
+    type: 'LOAD_REGIONS',
+    regions
+})
+
+export const loadCollection = (collection) => ({
+    type: 'LOAD_COLLECTION',
+    collection
+})

fd23fdd7da0af8f669909fa889c27820095940dd
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-27 15:40:10 -0700

    Create region component structure

---
+++ src/components/Regions/Regions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Regions.scss';
+
+const Regions = () => {
+    return (
+        <section key={objectid} className="piece-cont">
+        </section>
+    )
+}
+
+export default Regions;

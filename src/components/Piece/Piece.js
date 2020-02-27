b5ff4bfceceed8e014040c0eeb94089985b57cf1
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-26 21:15:01 -0700

    Interpolate props for labeling on Piece

--- src/components/Piece/Piece.js
+++ src/components/Piece/Piece.js
@@ -14,11 +14,13 @@ const Piece = ({
     medium,
     colors,
     creditline,
-    labeltext}) => {
+    labeltext
+    }) => {
+
     return (
         <section key={objectid} className="piece-cont">
-            <img alt={description} src={primaryimageurl} />
-            <article className="desc">
+            <img alt={description} src={primaryimageurl} className="piece-img"/>
+            <article className="piece-desc">
                 <header className="desc-header">
                     <h1>{title}</h1>
                     <h3>{century}</h3>
@@ -27,13 +29,10 @@ const Piece = ({
                     <p>{labeltext}</p>
                 </main>
                 <footer className="desc-footer">
-                    <ul>
-                        <li>{dated}</li>
-                        <li>{culture}</li>
-                        <li>{medium}</li>
-                        <li>{technique}</li>
-                        <li>{creditline}</li>
-                    </ul>
+                    <h4>{`Dated: ${dated}`}</h4>
+                    <h4>{`Medium: ${medium}`}</h4>
+                    <h4>{`Technique: ${technique}`}</h4>
+                    <h4>{creditline}</h4>
                 </footer>
             </article>
         </section>

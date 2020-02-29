3519c30a31f9bbbfb7cbc88733a4f6831cd09900
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-29 09:29:42 -0700

    Add recursive fetch iterations to handle pagination

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,20 +6,13 @@ import Loading from '../../components/Loading/Loading';
 import Timeline from '../../components/Timeline/Timeline';
 import { loadRegions } from '../../actions';
 import { connect } from 'react-redux';
-import { restructureArtPiece } from '../../utils/helpers';
-import { loadCollection } from '../../actions';
+import { restructureArtPiece, bucketArtByDate } from '../../utils/helpers';
+import { loadCollection, loadSubsqCollection } from '../../actions';
 import { Route, Switch } from 'react-router-dom';
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collection: []
-    }
-  }
-
-  retrieveCollection = async () => {
-    const collection = await apiCall('https://api.harvardartmuseums.org/object?apikey=b59b0050-58c4-11ea-b831-f76084e9f972&q=contextualtextcount:1&hasimage=1')
+  retrieveInitialCollection = async () => {
+    const collection = await apiCall('https://api.harvardartmuseums.org/object?apikey=b59b0050-58c4-11ea-b831-f76084e9f972&hasimage=1&place=2028429&classification=17&culture=American&size=100&sort=dateend&sortorder=desc')
 
     const rawCollectionResp = await collection.json();
 
@@ -28,31 +21,48 @@ export class App extends Component {
       return restrCollection;
     }, [])
 
-    return structuredCollectionData;
+    this.props.loadCollectionToStore(structuredCollectionData)
+    // console.log('firstCollResp', rawCollectionResp.info.pages > 1)
+    if (rawCollectionResp.info.pages > 1) {
+      this.retrieveSubseqCollections(rawCollectionResp)
+    }
   }
 
+  retrieveSubseqCollections = async (prevCollection) => {
+    const collection = await apiCall(`${prevCollection.info.next}`)
+
+    const rawCollectionResp = await collection.json();
+
+    const structuredCollectionData = rawCollectionResp.records.reduce((restrCollection, item) => {
+      restrCollection.push(restructureArtPiece(item))
+      return restrCollection;
+    }, []);
+
+    this.props.loadSubsqCollectionToStore(structuredCollectionData);
+    if (rawCollectionResp.info.page !== rawCollectionResp.info.pages) {
+      this.retrieveSubseqCollections(rawCollectionResp)
+    }
+  }
+
+
   componentDidMount() {
-    this.retrieveCollection()
-      .then(collectionData => {
-        this.setState({ collection: collectionData })
-        this.props.loadCollectionToStore(collectionData)
-      })
+    this.retrieveInitialCollection()
   }
 
   render() {
     return (
       <div className="App">
-        {!this.state.collection.length && <Loading />}
+        {!this.props.collections.length && <Loading />}
         <Route exact path="/" render={() => {
           return (
-              this.state.collection.length && <Timeline collection={this.state.collection} />
+            this.props.collections.length && <Timeline collection={this.props.collections} />
         )}} />
         <Route exact path='/piece/:id' render={({ match }) => {
-          const artPiece = this.state.collection.find(piece => piece.objectid === parseInt(match.params.id))
+          const artPiece = this.props.collections.collection.find(piece => piece.objectid === parseInt(match.params.id))
           return (
             <section className="App">
-              {!this.state.collection.length && <Loading />}
-              {this.state.collection.length && <Piece {...artPiece} />}
+              {!this.props.collections.collection.length && <Loading />}
+              {this.props.collections.collection.length && <Piece {...artPiece} />}
             </section>
           )
         }} />
@@ -63,9 +73,14 @@ export class App extends Component {
 
 export const mapDispatchToProps = dispatch => ({
   loadRegionsToStore: (regions) => { dispatch(loadRegions(regions)) },
-  loadCollectionToStore: (collection) => { dispatch(loadCollection(collection)) }
+  loadCollectionToStore: (collection) => { dispatch(loadCollection(collection)) },
+  loadSubsqCollectionToStore: (collection) => { dispatch(loadSubsqCollection(collection)) }
 });
 
-export default connect(null, mapDispatchToProps)(App)
+export const mapStateToProps = state => ({
+  collections: state.collections,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
 

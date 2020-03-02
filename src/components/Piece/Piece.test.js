ec205d9fcc1b3470986d7237681a38e222f03267
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 14:15:21 -0700

    Update Piece test with pass through data

--- src/components/Piece/Piece.test.js
+++ src/components/Piece/Piece.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Piece } from './Piece';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import { mockCollectionPrimary } from '../../utils/referenceData';
 
 describe('Piece', () => {
     it('should match a snapshot as expected', () => {
-        const wrapper = shallow(<Piece />)
+        const wrapper = shallow(<Piece {...mockCollectionPrimary}/>)
 
         expect(wrapper).toMatchSnapshot()
     })

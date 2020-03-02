94486b60c338923d1495cf78c66ac4c9b191f3a4
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 16:03:51 -0700

    Add invalid state testing

--- src/containers/Invalid/Invalid.test.js
+++ src/containers/Invalid/Invalid.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Invalid } from './Invalid';
+import { Invalid, mapStateToProps } from './Invalid';
 import { shallow } from 'enzyme';
 import { mockCollectionPrimary, mockCollectionSecondary } from '../../utils/referenceData';
 
@@ -8,5 +8,19 @@ describe('Invalid Path', () => {
         const wrapper = shallow(<Invalid collections={[mockCollectionPrimary, mockCollectionSecondary]} />)
 
         expect(wrapper).toMatchSnapshot()
+    })
+
+    describe('mapStateToProps', () => {
+        it('should return an array of collection objects as a prop', () => {
+            const mockState = {
+                region: 'Colorado',
+                collections: [mockCollectionPrimary, mockCollectionSecondary],
+            }
+            const expected = {
+                collections: [mockCollectionPrimary, mockCollectionSecondary]
+            }
+            const mappedProps = mapStateToProps(mockState);
+            expect(mappedProps).toEqual(expected)
+        })
     })
 })

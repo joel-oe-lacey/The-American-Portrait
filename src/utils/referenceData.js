97f8268698a01b76b75464d47b20f651661ff0c9
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 12:15:56 -0700

    Create mockData imports and test actions

--- src/utils/referenceData.js
+++ src/utils/referenceData.js
@@ -2,3 +2,57 @@ export const introText = ['People are trapped in history, and history is trapped
 'A continuation of that allegory, I believe, also speaks to the importance of remaining in touch with our collective history. It, in similar duality, is a part of us all.', 'The American Portrait looks to reunite us with a visual piece of that history. Utilizing the Harvard Museum API, you can view a timeline of historic photos by state, ten per decade where available.','While incomplete and historically biased, these photos offer us at very least a partial window into some of the lives, events, and adolescent places that would go on to form the nation as we know it. ']
 
 export const stateNames = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+export const mockCollectionPrimary = {
+    "title": "House and [blue] Car, Near Akron, Alabama",
+    "objectid": 354191,
+    "creditline": "Harvard Art Museums/Fogg Museum, Fund for the Acquisition of Photographs",
+    "century": "20th century",
+    "technique": "Dye transfer print",
+    "dateend": 1978,
+    "dated": "1978, printed 1991",
+    "people": [
+        {
+        "alphasort": "Christenberry, William",
+        "birthplace": "Tascaloosa, Alabama",
+        "name": "William Christenberry",
+        "prefix": null,
+        "personid": 62113,
+        "gender": "unknown",
+        "role": "Artist",
+        "displayorder": 1,
+        "culture": "American",
+        "displaydate": "born 1936",
+        "deathplace": null,
+        "displayname": "William Christenberry"
+        }
+    ],
+    "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:761794"
+}
+
+export const mockCollectionSecondary = {
+    "title": "Night Spot, Marion, Alabama",
+    "objectid": 354192,
+    "creditline": "Harvard Art Museums/Fogg Museum, Fund for the Acquisition of Photographs",
+    "century": "20th century",
+    "technique": "Dye transfer print",
+    "dateend": 1972,
+    "dated": "1972, printed 1991",
+    "people": [
+        {
+            "alphasort": "Christenberry, William",
+            "birthplace": "Tascaloosa, Alabama",
+            "name": "William Christenberry",
+            "prefix": null,
+            "personid": 62113,
+            "gender": "unknown",
+            "role": "Artist",
+            "displayorder": 1,
+            "culture": "American",
+            "displaydate": "born 1936",
+            "deathplace": null,
+            "displayname": "William Christenberry"
+        }
+    ],
+    "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:761793"
+}

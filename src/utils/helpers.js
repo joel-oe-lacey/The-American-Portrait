96b6f4659e76833e6548471100c002a558aefaac
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 11:14:57 -0700

    Update piece deconstruction and display for relevant fields

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,70 +1,26 @@
-export class ArtPiece {
-    primaryimageurl;
-    title;
-    objectid;
-    description;
-    century;
-    dated;
-    culture;
-    classification;
-    technique;
-    medium;
-    colors;
-    creditline;
-    labeltext;
-
-    constructor(rawArtData) {
-        Object.assign(this, rawArtData);
-    }
-};
-
 export const restructureArtPiece = (rawArtData) => {
     const {
         title,
-        titles,
         objectid,
-        description,
-        provenance,
-        commentary,
-        labeltext,
-        classification,
         creditline,
         century,
-        culture,
-        medium,
-        videos,
-        datebegin,
+        technique,
         dateend,
         dated,
-        period,
-        technique,
-        colors,
-        primaryimageurl,
-        images,
+        people,
+        primaryimageurl
     } = rawArtData;
 
     return {
         title,
-        titles,
         objectid,
-        description,
-        provenance,
-        commentary,
-        labeltext,
-        classification,
         creditline,
         century,
-        culture,
-        medium,
-        videos,
-        datebegin,
+        technique,
         dateend,
         dated,
-        period,
-        technique,
-        colors,
-        primaryimageurl,
-        images
+        people,
+        primaryimageurl
     }
 }
 

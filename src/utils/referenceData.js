3861e92c1dd5284a3d117ad8b39ee3280998b57e
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 16:04:08 -0700

    Add test reference data

--- src/utils/referenceData.js
+++ src/utils/referenceData.js
@@ -56,3 +56,206 @@ export const mockCollectionSecondary = {
     ],
     "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:761793"
 }
+
+export const mockRawCollection = {
+    "accessionyear": 2011,
+    "technique": "Gelatin silver print",
+    "mediacount": 0,
+    "edition": null,
+    "totalpageviews": 18,
+    "groupcount": 1,
+    "people": [
+        {
+            "alphasort": "Maisel, David",
+            "birthplace": "New York, NY",
+            "name": "David Maisel",
+            "prefix": null,
+            "personid": 56674,
+            "gender": "male",
+            "role": "Artist",
+            "displayorder": 1,
+            "culture": "American",
+            "displaydate": "born 1961",
+            "deathplace": null,
+            "displayname": "David Maisel"
+        }
+    ],
+    "objectnumber": "2011.366",
+    "colorcount": 10,
+    "lastupdate": "2020-02-28T05:59:32-0500",
+    "rank": 136014,
+    "imagecount": 1,
+    "description": null,
+    "dateoflastpageview": "2020-02-10",
+    "dateoffirstpageview": "2012-05-30",
+    "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:757415",
+    "colors": [
+        {
+            "percent": 0.1792393736017897,
+            "spectrum": "#2eb45d",
+            "color": "#323232",
+            "css3": "#2f4f4f",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.17154362416107383,
+            "spectrum": "#3db657",
+            "color": "#321919",
+            "css3": "#000000",
+            "hue": "Brown"
+        },
+        {
+            "percent": 0.16572706935123044,
+            "spectrum": "#3db657",
+            "color": "#4b4b4b",
+            "css3": "#2f4f4f",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.10286353467561521,
+            "spectrum": "#7866ad",
+            "color": "#646464",
+            "css3": "#696969",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.10241610738255033,
+            "spectrum": "#2eb45d",
+            "color": "#190000",
+            "css3": "#000000",
+            "hue": "Black"
+        },
+        {
+            "percent": 0.06241610738255034,
+            "spectrum": "#8c5fa8",
+            "color": "#c8c8c8",
+            "css3": "#c0c0c0",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.06098434004474273,
+            "spectrum": "#8362aa",
+            "color": "#7d7d7d",
+            "css3": "#808080",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.05821029082774049,
+            "spectrum": "#2eb45d",
+            "color": "#191900",
+            "css3": "#000000",
+            "hue": "Brown"
+        },
+        {
+            "percent": 0.047248322147651005,
+            "spectrum": "#8c5fa8",
+            "color": "#afafaf",
+            "css3": "#a9a9a9",
+            "hue": "Grey"
+        },
+        {
+            "percent": 0.04022371364653244,
+            "spectrum": "#8761aa",
+            "color": "#969696",
+            "css3": "#a9a9a9",
+            "hue": "Grey"
+        }
+    ],
+    "dated": "1988, printed 1990",
+    "contextualtextcount": 0,
+    "copyright": "© David Maisel",
+    "period": null,
+    "accessionmethod": "Partial Gift/Partial Purchase",
+    "url": "https://www.harvardartmuseums.org/collections/object/337892",
+    "provenance": "David Maisel, 1990, to; [Gary Schneider and John Erdman, New York] (1990-2011), gift/sale; to Harvard Art Museums, 2011. Note: Printed in collaboration with the artist or the artist's estate and retained by Schneider/Erdman per agreement.",
+    "images": [
+        {
+            "height": 2550,
+            "iiifbaseuri": "https://ids.lib.harvard.edu/ids/iiif/400822131",
+            "baseimageurl": "https://nrs.harvard.edu/urn-3:HUAM:757415",
+            "width": 2546,
+            "publiccaption": null,
+            "idsid": 400822131,
+            "displayorder": 1,
+            "format": "image/jpeg",
+            "copyright": "President and Fellows of Harvard College",
+            "imageid": 446866,
+            "renditionnumber": "757415"
+        }
+    ],
+    "publicationcount": 1,
+    "objectid": 337892,
+    "culture": "American",
+    "verificationleveldescription": "Good. Object is well described and information is vetted",
+    "standardreferencenumber": null,
+    "worktypes": [
+        {
+            "worktypeid": "259",
+            "worktype": "photograph"
+        }
+    ],
+    "department": "Department of Photographs",
+    "state": null,
+    "markscount": 1,
+    "contact": "am_moderncontemporary@harvard.edu",
+    "titlescount": 1,
+    "id": 337892,
+    "title": "Tongue of Matanuska Glacier",
+    "verificationlevel": 3,
+    "division": "Modern and Contemporary Art",
+    "style": null,
+    "commentary": null,
+    "relatedcount": 0,
+    "datebegin": 1988,
+    "labeltext": null,
+    "totaluniquepageviews": 14,
+    "dimensions": "71.6 x 71.5 cm (28 3/16 x 28 1/8 in.)\r\nsheet: 98.5 x 74.7 cm (38 3/4 x 29 7/16 in.)",
+    "exhibitioncount": 1,
+    "techniqueid": 123,
+    "dateend": 1988,
+    "creditline": "Harvard Art Museums/Fogg Museum, Schneider/Erdman Printer's Proof Collection, partial gift, and partial purchase through the Margaret Fisher Fund",
+    "imagepermissionlevel": 0,
+    "signed": null,
+    "periodid": null,
+    "century": "20th century",
+    "classificationid": 17,
+    "medium": "Toned gelatin silver print",
+    "peoplecount": 1,
+    "accesslevel": 1,
+    "classification": "Photographs",
+    "seeAlso": [
+        {
+            "id": "https://iiif.harvardartmuseums.org/manifests/object/337892",
+            "type": "IIIF Manifest",
+            "format": "application/json",
+            "profile": "http://iiif.io/api/presentation/2/context.json"
+        }
+    ]
+}
+
+export const mockFilteredCollection = {
+    "title": "Tongue of Matanuska Glacier",
+    "objectid": 337892,
+    "creditline": "Harvard Art Museums/Fogg Museum, Schneider/Erdman Printer's Proof Collection, partial gift, and partial purchase through the Margaret Fisher Fund",
+    "century": "20th century",
+    "technique": "Gelatin silver print",
+    "dateend": 1988,
+    "dated": "1988, printed 1990",
+    "people": [
+        {
+            "alphasort": "Maisel, David",
+            "birthplace": "New York, NY",
+            "name": "David Maisel",
+            "prefix": null,
+            "personid": 56674,
+            "gender": "male",
+            "role": "Artist",
+            "displayorder": 1,
+            "culture": "American",
+            "displaydate": "born 1961",
+            "deathplace": null,
+            "displayname": "David Maisel"
+        }
+    ],
+    "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:757415"
+}

a1d3a4ba3ba776de933edc0ebd64e8038a731c9f
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 16:03:21 -0700

    Add About dispatch testing

--- src/containers/About/About.test.js
+++ src/containers/About/About.test.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { About } from './About';
+import { About, mapDispatchToProps } from './About';
 import { shallow } from 'enzyme';
+import { loadRegion } from '../../actions';
 
 describe('About', () => {
     it('should match a snapshot as expected', () => {
         const wrapper = shallow(<About />)
         
         expect(wrapper).toMatchSnapshot()
+    })
+
+    describe('mapDispatchToProps', () => {
+        it('call dispatch with the loadRegion action when loadRegionToStore is called', () => {
+            const mockDispatch = jest.fn();
+            const region = "Colorado";
+            const actionToDispatch = loadRegion(region)
+            const mappedProps = mapDispatchToProps(mockDispatch)
+
+            mappedProps.loadRegionToStore(region)
+
+            expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+        })
     })
 })

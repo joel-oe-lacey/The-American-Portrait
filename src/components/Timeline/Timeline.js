a1f3bd7906f93913642bb9735cfaf14cfaf1758f
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-01 17:53:08 -0700

    Fix timeline scaling and display bugs

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -43,19 +43,25 @@ export class Timeline extends Component {
         return (
         <section className="carousel">
             <section className="carousel-disp">
+                    {(i !== 0) && <img className='carousel-img-prev'
+                        src={collection[i-1].primaryimageurl}
+                        alt={collection[i-1].description} />}
                 <Link to={`piece/${collection[i].objectid}`} className='carousel-link' >
                     <img className='carousel-img' 
                     src={collection[i].primaryimageurl} 
                     alt={collection[i].description}  />
                 </Link>
+                    {(i !== endIndex) && <img className='carousel-img-next'
+                        src={collection[i + 1].primaryimageurl}
+                        alt={collection[i + 1].description} />}
             </section>
             <h1 className="carousel-title">{collection[i].title}</h1>
             <section className="carousel-timeline">
-                <button className='carousel-arrow-left' onClick={() => this.changePiece('left')}>←</button>
+                    <button className='carousel-arrow-left' onClick={() => this.changePiece('left')}>←</button>
                     <h2 className="timeline-prev">{i ? collection[i-1].dateend : collection[i].dateend}</h2>
         <h1 className="timeline-curr">{collection[i].dateend}</h1>
                     <h2 className="timeline-next">{i === endIndex ? collection[i].dateend : collection[i + 1].dateend}</h2>
-                <button className='carousel-arrow-right' onClick={() => this.changePiece('right')}>→</button>
+                    <button className='carousel-arrow-right' onClick={() => this.changePiece('right')}>→</button>
             </section>
         </section>
     )}

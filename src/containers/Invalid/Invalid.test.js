422e90ca164284067e56e053cc4ac170663ecb4f
Joel Lacey <joellaceybusiness@gmail.com>
2020-03-02 14:40:14 -0700

    Add mock data pass through for Invalid test

--- src/containers/Invalid/Invalid.test.js
+++ src/containers/Invalid/Invalid.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Invalid } from './Invalid';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import { mockCollectionPrimary, mockCollectionSecondary } from '../../utils/referenceData';
 
 describe('Invalid Path', () => {
     it('should match a snapshot as expected', () => {
-        const wrapper = shallow(<Invalid />)
+        const wrapper = shallow(<Invalid collections={[mockCollectionPrimary, mockCollectionSecondary]} />)
 
         expect(wrapper).toMatchSnapshot()
     })
